c53415454256efb5a2ae277211653fb70e9fe486
Jeff Davis <jeff.davis@gmail.com>
2012-01-07 19:40:32 -0500

    probs 8, 14, 15

---
+++ prob014_test.go
@@ -0,0 +1,120 @@
+package euler
+
+import (
+	"testing"
+	"log"
+)
+
+func collatzMemo(lvec []int, sc []int64, off int) {
+	//log.Print(off, sc)
+	lvn := int64(len(lvec))
+
+	for i, n := range sc {
+		if n < lvn {
+			lvec[n] = len(sc) + off - i
+		}
+	}
+}
+
+func Collatz(n int64, lvec []int, sc []int64) (int, []int64) {
+	if lvec[n] != 0 {
+		return lvec[n], sc[:0]
+	}
+
+	lvn := int64(len(lvec))
+	sc = sc[:1]
+	sc[0] = n
+
+	for n != 1 {
+		if n < lvn && lvec[n] != 0 {
+			collatzMemo(lvec, sc, lvec[n]-1)
+			return lvec[sc[0]], sc
+		}
+
+		if n&1 == 0 {
+			n >>= 1
+		} else {
+			//n = n<<1 + n + 1
+			n = 3*n + 1
+			if n < 0 {
+				log.Panic("overflow")
+			}
+		}
+
+		sc = append(sc, n)
+	}
+
+	// If we fell out we never encountered a memoized seq.
+	collatzMemo(lvec, sc, 0)
+	return len(sc), sc
+}
+
+func prob014() int {
+	N := int64(1000000)
+	lvec := make([]int, N+1)
+	sc := make([]int64, 1000)
+	l := 0
+	imax, max := 0, 0
+	for i := int64(1); i < N; i++ {
+		l, sc = Collatz(i, lvec, sc)
+		if l > max {
+			max = l
+			imax = int(i)
+		}
+	}
+
+	return imax
+}
+
+func prob014nomem() int {
+	N := 1000000
+	imax, max := 0, 0
+	for i := 1; i < N; i++ {
+		l := 1
+		n := int64(i)
+		for n != 1 {
+			l++
+			if n&1 == 0 {
+				n >>= 1
+			} else {
+				//n = n<<1 + n + 1
+				n = 3*n + 1
+				if n < 0 {
+					log.Panic("overflow")
+				}
+			}
+		}
+		if l > max {
+			max = l
+			imax = i
+		}
+	}
+
+	return imax
+}
+
+func TestProb014(t *testing.T) {
+	imax := prob014()
+	if imax != 837799 {
+		t.Error("Prob014 failed ", imax)
+	}
+}
+
+func BenchmarkProb014(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		prob014()
+	}
+}
+
+func TestProb014nomem(t *testing.T) {
+	imax := prob014nomem()
+	if imax != 837799 {
+		t.Error("Prob014 failed ", imax)
+	}
+}
+
+func BenchmarkProb014nomem(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		prob014nomem()
+	}
+}
---
+++ prob015_test.go
@@ -0,0 +1,35 @@
+package euler
+
+import (
+	"testing"
+	"log"
+	"big"
+)
+
+func prob015() string {
+	// Just use math for this.
+	// its (2N)!/(N!N!) so in this case 40!/20!/20!
+	// All praise Pascal...
+	z := big.NewInt(0)
+	z = z.MulRange(1, 40)
+	z2 := big.NewInt(0)
+	z2 = z2.MulRange(1, 20)
+	z = z.Div(z, z2)
+	z = z.Div(z, z2)
+
+	return z.String()
+
+}
+
+func TestProb015(t *testing.T) {
+	if prob015() != "137846528820" {
+		log.Print(prob015())
+		t.Error("Prob015 failed")
+	}
+}
+
+func BenchmarkProb015(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		prob015()
+	}
+}
